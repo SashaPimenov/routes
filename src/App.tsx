ee3ce17817bb0aacc566a915c60fe3353b992d17
Sasha Pimenov <sasha.pimenov.2002@inbox.ru>
2023-11-05 12:18:58 +0500

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/mainPage/main-page";
+import MapPage from "./pages/map/map-page";
+import LoginPage from "./pages/loginPage/login-page";
 
 function App() {
   return (
-    <div className="wrapper_root">
-      <div>
-        <div className="sideBar">
-          <div>
-            <a href={`/map`}>map</a>
-          </div>
-          <div>
-            <a href={`/login`}>login</a>
-          </div>
+    <>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="map" element={<MapPage />}></Route>
+          <Route path="login" element={<LoginPage />}></Route>
+        </Routes>
+      </div>
+      <div className="sideBar">
+        <div>
+          <a href={`/map`}>map</a>
+        </div>
+        <div>
+          <a href={`/login`}>login</a>
         </div>
       </div>
-      <div className="main">
-        <Outlet />
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import LoginPage from "./pages/loginPage/login-page";
-import MapPage from "./pages/map/map-page";
-import MainPage from "./pages/mainPage/main-page";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                path: "/",
-                element: <MainPage />
-            },
-            {
-                path: "/map",
-                element: <MapPage />
-            },
-            {
-                path: "/login",
-                element: <LoginPage />
-            },
-        ]
-    }
-])
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
