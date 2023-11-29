11ec34b6a57562bf1ebc998508e4ddd8b92aec65
Sasha Pimenov <sasha.pimenov.2002@inbox.ru>
2023-11-29 18:34:12 +0500

    pravki

--- src/components/forMapPage/oneRoutComponentMap.tsx
+++ src/components/forMapPage/oneRoutComponentMap.tsx
@@ -1,37 +1,50 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import "./oneRoutComponentMap.css";
 
-
 interface IProp {
-    id: number;
-    title: string;
-    description: string;
-
+  id: number;
+  title: string;
+  description: string;
+  setOpenFunc?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function OneRoutComponentMap({id, title, description}: IProp) {
-
-    return (
-        <>
-            <div className="wrapperOneRouteMap">
-                <div className="oneRouteMap">
-                    <div className="cardMap">
-                        <div className="headerCardMap">
-                            <div className={"characterRouteMap"}>
-                                <div className={"routeTimeAndDistanceMap routeTimeMap"}>2 часа</div>
-                                <div className={"routeTimeAndDistanceMap routeDistanceMap"}>10 км</div>
-                            </div>
-                            <p className="headerCardTextMap">{title}</p>
-                        </div>
-                        <div className="footerCardMap">
-                            <p className="footerCardTextMap">{description}</p>
-                        </div>
-                    </div>
-                    <div className="oneRouteDivMap">
-                        <img className={"oneRouteDivImageMap"} src={require("../../assets/ekb.jpg")}/>
-                    </div>
+export default function OneRoutComponentMap({
+  id,
+  title,
+  description,
+  setOpenFunc,
+}: IProp) {
+  return (
+    <>
+      <div
+        className="wrapperOneRouteMap"
+        onClick={() => (setOpenFunc ? setOpenFunc(true) : null)}
+      >
+        <div className="oneRouteMap">
+          <div className="cardMap">
+            <div className="headerCardMap">
+              <div className={"characterRouteMap"}>
+                <div className={"routeTimeAndDistanceMap routeTimeMap"}>
+                  2 часа
                 </div>
+                <div className={"routeTimeAndDistanceMap routeDistanceMap"}>
+                  10 км
+                </div>
+              </div>
+              <p className="headerCardTextMap">{title}</p>
             </div>
-        </>
-    );
+            <div className="footerCardMap">
+              <p className="footerCardTextMap">{description}</p>
+            </div>
+          </div>
+          <div className="oneRouteDivMap">
+            <img
+              className={"oneRouteDivImageMap"}
+              src={require("../../assets/ekb.jpg")}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
